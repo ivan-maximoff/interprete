bcf1fd51c40ffe9154d7f90f7ce828f60746c350
MatiBartellone <matias.bartellone@gmail.com>
2024-05-30 12:43:15 -0300

    agrego reducciones

--- main.scala
+++ main.scala
@@ -1,26 +1,134 @@
 package lambda
 
-import interprete.interpretarEcuacion
-import lector.leerEcuacion
-import procesador.procesar
+def leerEcuacion(ecuacion: String): List[Operador | String] = {
+    val ops = ecuacion.split("").toList
+    _leerEcuacionRec(ops)
+}
 
-import java.util.Scanner
-import scala.io.StdIn.readLine
+sealed trait Expresion
 
-object Main {
-    def main(args: Array[String]): Unit = {
-        leerInput()
+case class Variable(nombre: String) extends Expresion
+
+case class Abstraccion(variable: Variable, cuerpo: Expresion) extends Expresion
+
+case class Aplicacion(funcion: Expresion, argumento: Expresion) extends Expresion
+
+enum Operador {
+    case LAMBDA, PUNTO, PAREN_IZQ, PAREN_DER, ESPACIO
+}
+
+private def _leerEcuacionRec(tokens: List[String]): List[Operador | String] = tokens match {
+    case Nil => List()
+    case x :: xs => {
+        val op = x match {
+            case "λ" => Operador.LAMBDA
+            case " " => Operador.ESPACIO
+            case "." => Operador.PUNTO
+            case "(" => Operador.PAREN_IZQ
+            case ")" => Operador.PAREN_DER
+            case _ => x
+        }
+        op :: _leerEcuacionRec(xs)
     }
 }
 
+def encontrarAplicacion(ecuacion: List[Operador | String], aplicaciones: Int, index: Int): Int = {
+    ecuacion match {
+        case x :: xs =>
+            x match {
+                case Operador.ESPACIO if aplicaciones == 0 => index
+                case Operador.PAREN_IZQ => encontrarAplicacion(xs, aplicaciones + 1, index + 1)
+                case Operador.PAREN_DER => encontrarAplicacion(xs, aplicaciones - 1, index + 1)
+                case _ => encontrarAplicacion(xs, aplicaciones, index + 1)
+            }
+    }
+}
+
+def interpretarEcuacion(ecuacion: List[Operador | String]): Expresion = {
+    _interpretarEcuacion(ecuacion)
+}
+
+private def _interpretarEcuacion(ecuacion: List[Operador | String]): Expresion = {
+    ecuacion match {
+        case Nil => Variable("")
+        case (s: String) :: xs => Variable(s)
+        case Operador.LAMBDA :: (nombre: String) :: Operador.PUNTO :: xs =>
+            val variable = Variable(nombre)
+            Abstraccion(variable, cuerpo = _interpretarEcuacion(xs))
+        case Operador.PAREN_IZQ :: xs =>
+            val indexSeparacion = encontrarAplicacion(xs.init, 0, 1)
+            val (lista1, lista2) = xs.splitAt(indexSeparacion)
+            Aplicacion(funcion = _interpretarEcuacion(lista1), argumento = _interpretarEcuacion(lista2))
+    }
+}
+
+def reducir(expresion: Expresion, reductor: Expresion => Expresion): Expresion = {
+    reductor(expresion)
+}
+
+def sustitucion(vl: Variable, arbol: Expresion, sustituyente: Expresion): Expresion = {
+    arbol match {
+        case v: Variable => v
+        case Abstraccion(e1, e2) => Abstraccion(e1, sustitucion(vl, e2, sustituyente))
+        case Aplicacion(e1, e2) => Aplicacion(sustitucion(vl, e1, sustituyente), sustitucion(vl, e2, sustituyente))
+    }
+}
+/*
+def convertir(v: Variable): Variable = {
+
+}
+    case Abstraccion(e1, e2) => {
+        e1 match {
+            case vl => Abstraccion(convertir(e1), sustitucion(vl, e2))
+            case _ => Abstraccion(e1, sustitucion(vl, e2))
+        }
+    }*/
+
+def callByName(expresion: Expresion): Expresion = {
+    expresion match {
+        case Variable(v) => expresion
+        case Abstraccion(e1, e2) => Abstraccion(e1, callByName(e2))
+        case Aplicacion(e1, e2) =>
+            e1 match {
+                case Abstraccion(vl, e) => sustitucion(vl, e, e2)
+                case _ => Aplication(callByName(e1), callByName(e2))
+            }
+    }
+}
+/*
+def callByValue(expresion: Expresion): Expresion = {
+    expresion match {
+        case Variable(v) => expresion
+        case Abstraccion(vl, e2) => callByValue(e2)
+        case Aplicacion(e1, e2) =>
+            callByValue(e1)
+            e1 match {
+                case Abstraccion(vl, e) =>
+                    callByValue(e2)
+                    sustitucion(vl, e, e2)
+            }
+            callByValue(e2) /*?hace falta?*/
+    }
+}
+*/
+object Main {
+    def main(args: Array[String]): Unit = {
+        //leerInput()
+        val ecuacion = leerEcuacion("(λx.λx.(y x) z)")
+        val expresion = interpretarEcuacion(ecuacion)
+        val resultado = reducir(expresion, callByName)
+    }
+}
+/*
 def leerInput(): Unit = {
     val input = scala.io.StdIn.readLine()
-    input match 
+    input match {
         case null =>
         case _ =>
             val ecuacionParseada = leerEcuacion(input)
             val expresion = interpretarEcuacion(ecuacionParseada)
-            val resultado = procesar(expresion)
+            val resultado = reducir(expresion, callByName)
             printf("Resultado = %f \n", resultado)
             leerInput()
-}
+    }
+}*/
--- procesador/procesador.scala
+++ procesador/procesador.scala
@@ -2,9 +2,58 @@ package procesador
 
 import modelo.expresion.*
 
-def procesar(expresion: Expresion): Float = {
-    expresion match
-        case Variable(v) => v
-        case Abstraccion(e1, e2) => 
-        case Aplicacion(e1, e2) => 
+def reducir(expresion: Expresion, reductor: Expresion => Expresion): Expresion = {
+    reductor(expresion)
 }
+
+def sustitucion(vl: Variable, arbol: Expresion, sustituyente: Expresion): Expresion = {
+    arbol match {
+        case v: Variable => v
+        case Abstraccion(e1, e2) => Abstraccion(e1, sustitucion(vl, e2, sustituyente))
+        case Aplicacion(e1, e2) => Aplicacion(sustitucion(vl, e1, sustituyente), sustitucion(vl, e2, sustituyente))
+    }
+}
+/*
+def convertir(v: Variable): Variable = {
+
+}
+    case Abstraccion(e1, e2) => {
+        e1 match {
+            case vl => Abstraccion(convertir(e1), sustitucion(vl, e2))
+            case _ => Abstraccion(e1, sustitucion(vl, e2))
+        }
+    }*/
+
+def callByName(expresion: Expresion): Expresion = {
+    expresion match {
+        case Variable(v) => expresion
+        case Abstraccion(e1, e2) => Abstraccion(e1, callByName(e2))
+        case Aplicacion(e1, e2) =>
+            callByName(e1)
+            e1 match {case Abstraccion(vl, e) => sustitucion(vl, e, e2)}
+            callByName(e2)
+    }
+}
+
+def callByValue(expresion: Expresion): Expresion = {
+    expresion match {
+        case Variable(v) => expresion
+        case Abstraccion(vl, e2) => callByValue(e2)
+        case Aplicacion(e1,e2) =>
+            callByValue(e1)
+            e1 match {
+                case Abstraccion(vl, e) =>
+                    callByValue(e2)
+                    sustitucion(vl, e, e2)
+            }
+            callByValue(e2) /*?hace falta?*/
+    }
+}
+/*
+def variablesLibes(expresion: Expresion): Expresion = {
+    expresion match {
+        case Variable(v) => expresion
+        case Abstraccion(vl, e2) => variablesLibes(e2) - {vl}
+        case Aplicacion(e1, e2) => variablesLibes(e1) + variablesLibes(e2)
+    }
+}*/
